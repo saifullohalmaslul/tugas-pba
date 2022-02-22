076f43433d8bcb12fd3b5e4d459a23b1f0a7d42d
Saifulloh Almaslul <92070797+saifullohalmaslul@users.noreply.github.com>
2022-02-22 21:46:54 +0700

    Init + 2a

---
+++ 2a/src/seg_punkt.py
@@ -0,0 +1,13 @@
+from nltk import text
+import nltk.tokenize.punkt
+
+file = open("../res/sentences.txt", "r")
+text = file.read()
+text = text.replace("\n", " ")
+
+tokenizer = nltk.data.load('../res/indo.pickle')
+segmented = tokenizer.tokenize(text)
+
+out = open("../res/segmented.txt", "w")
+for sentence in segmented:
+	out.write(sentence + "\n")
