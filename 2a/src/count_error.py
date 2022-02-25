d00325afcb5ce00af1e24747eec4ae7dcc1b1f0c
Saifulloh Almaslul <92070797+saifullohalmaslul@users.noreply.github.com>
2022-02-25 14:51:55 +0700

    Fix count_error, cari batasan kalimat menggunakan find/rfind

--- 2a/src/count_error.py
+++ 2a/src/count_error.py
@@ -1,10 +1,15 @@
-from xlwt import Workbook, Worksheet
+from os import error
+from xlwt import Workbook
 
 source = open("../res/sentences.txt", "r")
 result = open("../res/segmented.txt", "r")
 
 good_sentences = source.read()
+if not good_sentences.endswith('\n'): good_sentences += '\n'
 punkt_sentences = result.read()
+
+if not len(good_sentences) == len(punkt_sentences):
+	raise Exception("Panjang karakter kedua file berbeda.")
 
 source.close()
 result.close()
@@ -17,16 +22,22 @@ ws.write(0, 2, label='hasil punkt')
 err_count = 0
 stc_start = 0
 stc_end = 0
-for char in good_sentences:
-	stc_end += 1
-	if char == '\n':
-		good_stc = good_sentences[stc_start:stc_end]
-		punkt_stc = punkt_sentences[stc_start:stc_end]
-		if good_stc != punkt_stc:
-			err_count += 1
-			ws.write(err_count, 0, label=err_count)
-			ws.write(err_count, 1, label=good_stc.rstrip())
-			ws.write(err_count, 2, label=punkt_stc.rstrip())
-		stc_start = stc_end
+while stc_end != -1:
+	stc_end = good_sentences.find('\n', stc_start)
+	good_stc = good_sentences[max(0, stc_start-1) : stc_end+1]
+	punkt_stc = punkt_sentences[max(0, stc_start-1) : stc_end+1]
+	if good_stc != punkt_stc:
+		punkt_stc = punkt_sentences[punkt_sentences.rfind('\n', 0, stc_start)+1 : punkt_sentences.find('\n', stc_end)+1]
+
+		err_count += 1
+		print('- {0} ---------------------------------------------'.format(err_count))
+		print('\033[92m' + good_stc + '\033[0m')
+		print('\033[91m' + punkt_stc + '\033[0m')
+		ws.write(err_count, 0, label=err_count)
+		ws.write(err_count, 1, label=good_stc.rstrip())
+		ws.write(err_count, 2, label=punkt_stc.rstrip()) #TODO: buat tanda kesalahan segmentasi
+	stc_start = stc_end + 1
+
+print("Total error {0} dari {1} kalimat".format(err_count, good_sentences.count('\n')))
 
 wb.save('../res/punkt_error.xls')
