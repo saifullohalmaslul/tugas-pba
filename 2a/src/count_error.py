a93bc734b24f29b95558915a815dc1f6cb00f8f7
Saifulloh Almaslul <92070797+saifullohalmaslul@users.noreply.github.com>
2022-03-06 13:51:01 +0700

    Perbaiki deteksi awal/akhir kalimat hasil

--- 2a/src/count_error.py
+++ 2a/src/count_error.py
@@ -7,9 +7,9 @@ good_sentences = source.read()
 if not good_sentences.endswith('\n'): good_sentences += '\n'
 punkt_sentences = result.read()
 
-if not len(good_sentences) == len(punkt_sentences):
-	print("source {0}, result {1}".format(len(good_sentences), len(punkt_sentences)))
-	raise Exception("Panjang karakter kedua file berbeda.")
+# if not len(good_sentences) == len(punkt_sentences):
+# 	print("source {0}, result {1}".format(len(good_sentences), len(punkt_sentences)))
+# 	raise Exception("Panjang karakter kedua file berbeda.")
 
 source.close()
 result.close()
@@ -26,13 +26,18 @@ ws.write(0, 1, label='kalimat asli', style=heading)
 ws.write(0, 2, label='hasil punkt', style=heading)
 err_count = 0
 stc_start = 0
-stc_end = 0
+stc_end = good_sentences.find('\n', stc_start)
 while stc_end != -1:
-	stc_end = good_sentences.find('\n', stc_start)
 	good_stc = good_sentences[stc_start : stc_end]
-	punkt_stc = punkt_sentences[punkt_sentences.rfind('\n', 0, stc_start)+1 : punkt_sentences.find('\n', stc_end-1)]
+	p_start = punkt_sentences.rfind('\n', 0, stc_end)+1
+	p_end = punkt_sentences.find('\n', stc_end-1)
+	punkt_stc = punkt_sentences[p_start : p_end]
 
 	if good_stc != punkt_stc:
+		
+		while len(good_stc) > len(punkt_stc):
+			p_start = punkt_sentences.rfind('\n', 0, p_start-1)+1
+			punkt_stc = punkt_sentences[p_start : p_end]
 		err_count += 1
 		print('-[{0}]---------------------------------------------'.format(err_count))
 		print('\033[92m' + good_stc + '\033[0m')
@@ -42,6 +47,8 @@ while stc_end != -1:
 		ws.write(err_count, 1, label=good_stc)
 		ws.write(err_count, 2, label=punkt_stc) #TODO: buat tanda kesalahan segmentasi
 	stc_start = stc_end + 1
+	stc_end = good_sentences.find('\n', stc_start)
+
 
 stc_count = good_sentences.count('\n')
 err_rate = err_count / stc_count * 100
